d9cc419b351eb82269763ac4a9f7ce90ca1217db
ethan-strauss <91496002+ethan-strauss@users.noreply.github.com>
2025-04-08 13:00:49 -0400

    update
    updated files for utilities and HOA

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const form = document.getElementById("mortgage-form");
 const inputAmount = document.getElementById("amount");
 const inputRate = document.getElementById("rate");
 const inputYears =  document.getElementById("years");
+const inputUtilities =  document.getElementById("utilities");
+const inputHOA =  document.getElementById("hoa");
 const result =  document.getElementById("result");
 
 form.addEventListener("submit", function(event) {
@@ -9,16 +11,24 @@ form.addEventListener("submit", function(event) {
     const amount = parseFloat(inputAmount.value);
     const rate = parseFloat(inputRate.value);
     const years = parseFloat(inputYears.value);
+    let utilities = parseFloat(inputUtilities.value);
+    if( isNaN(utilities) ){
+        utilities = 0;
+    };
+    let hoa = parseFloat(inputHOA.value);
+    if (isNaN(hoa)){
+        hoa = 0
+    };
 
     const interest = (rate / 100) / 12;
     const totalPayments = years * 12;
+    let monthlyPayment;
 
     if (rate === 0){
-        let monthlyPayment;
-        monthlyPayment = amount / totalPayments;
+        monthlyPayment =( amount / totalPayments) + utilities + hoa;
     } else {
         const factor = Math.pow(1 + interest, totalPayments);
-        monthlyPayment = amount * (interest * factor) / (factor - 1);
+        monthlyPayment = amount * (interest * factor) / (factor - 1) + utilities + hoa;
     }
     result.textContent = "Monthly Payment: $" + monthlyPayment.toFixed(2);
 
